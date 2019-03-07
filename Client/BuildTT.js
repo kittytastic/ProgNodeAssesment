85a83197683148160ccd3085c99aeb1fcfbcfd73
= <jack@theslingsbys.com>
2019-03-07 21:10:31 +0000

    Cleaned up and commented table generator js

--- Client/BuildTT.js
+++ Client/BuildTT.js
@@ -1,82 +1,98 @@
 function generateHTML(tt_json){
     var tt_obj = JSON.parse(tt_json); 
 
-    console.log(tt_obj)
-    console.log(tt_json)
-
     var temp = find_time_limits(tt_obj)
     var start = temp[0];
     var end = temp[1];
-    
+    var step = 0.25
     
     var n_rows = tt_obj.days.length;
-  
-    
-    var step = 0.25
-  
-
     var n_cols = (end - start) / step
     console.log("Start:"+start)
     console.log("End: "+end)
     console.log("n_rows: "+n_rows)
     console.log("n_col: "+n_cols)
 
+
+    // Declare table beginning and end
     var table_start = "<table class = 'ttable ui tablet computer only'><tbody>"
     var table_end = "</tbody></table>"
   
+    // Start building table
     var table_html = ""
     table_html += table_start
     
-    table_html += "<tr>"
+    
 
+    // ----------------- Add row headings (times) ----------------- 
     col_per_hour = 1/step;
     start_pad = (Math.ceil(start)-start) / step
+    
+    table_html += "<tr>"
     table_html += "<td colspan = '"+start_pad+"' class='tt_headers'></td>"
 
     for(let i = start_pad; i<n_cols; i+=col_per_hour){
         rel_time = start + i *step;
-        if (Math.round(rel_time)==rel_time){
-            table_html+="<td colspan = '"+col_per_hour+"' class='tt_headers'><div>"+rel_time+"</div></td>"
-        }else{
-            console.log("Not sure why we are in this loop")
-            table_html+="<td></td>"
+
+        if (Math.round(rel_time)!=rel_time){
+            error("Trying to add time heading that aren't a whole number")
         }
         
+        table_html+="<td colspan = '"+col_per_hour+"' class='tt_headers'><div>"+rel_time+"</div></td>"
+       
     }
 
-    /* WHat happens if the finish time isn't a whole number */
-
     table_html += "</tr>"
+
+    /* What happens if the finish time isn't a whole number */
+
+    // ----------------- Add days and sessions -----------------
+    
     
       for(let i = 0 ; i<n_rows; i++){
         table_html += "<tr>"
-        let j = start;
-        while(j < end){
-            //related_time = start + j*step; 
-            related_time = j;
-            var sesh = is_start_of_time_slot(tt_obj, related_time, i);
-            if(sesh>-1){
-                console.log("Session "+tt_obj.days[i][sesh].session+" is at this time: "+related_time+ " on day: "+i)
-                let dur = tt_obj.days[i][sesh].end - tt_obj.days[i][sesh].start
+        let time_of_day = start;
+        while(time_of_day < end){
+            let session_id = is_start_of_time_slot(tt_obj, time_of_day, i);
+            
+            // If this time is a session
+            if(session_id>-1){
+
+                // Calculate duration of session
+                let dur = tt_obj.days[i][session_id].end - tt_obj.days[i][session_id].start
+                
+                // Check for errors
                 if(dur<=0){
-                    j += step;
+                    time_of_day += step;
                     table_html += "<td class = 'session-none'></td>";
-                    warning("ERROR rendering day, session starts after it ends")
+                    error("rendering day, session starts after it ends")
                 }else{
+                    // No errors
+                    // Calculate width of session and add to output html
                     let width = dur/step;
-                    console.log("Adding session width: "+width);
-                    table_html += "<td class = 'session-"+tt_obj.days[i][sesh].session+"' colspan='"+width+"'></td>"
-                    j = tt_obj.days[i][sesh].end
+                    table_html += "<td class = 'session-"+tt_obj.days[i][session_id].session+"' colspan='"+width+"'></td>"
+                    
+                    // Set next time to look at at the end of this session
+                    time_of_day = tt_obj.days[i][session_id].end
                 }
+            
             }else{
+                // No session on add blank box 
                 table_html += "<td class = 'session-none'></td>";
-                j += step;
+                time_of_day += step;
             }
         }
+
+        // Done for the day
         table_html += "</tr>"
       }
   
+    // Add close table
     table_html += table_end
+
+    
+    
+
 
     return generateCSS(tt_obj)+table_html
 
@@ -130,36 +146,5 @@ function find_time_limits(tt_data){
     console.log("Latest End "+largest);
 
     return [smallest, largest]
-
-}
-
-
-function generateTestHTML(){
-
-    var start = 6.0
-    var end = 18.0
-    var rows = 7
-  
-    var step = 0.25
-  
-    var col = (end- start) / step
-  
-    var table_start = "<table class = 'ttable ui tablet computer only'><tbody>"
-    var table_end = "</tbody></table>"
-  
-    var table_html = ""
-    table_html += table_start
-  
-      for(i = 0 ; i<rows; i++){
-        table_html += "<tr>"
-        for(j =0; j<col; j++){
-          table_html += "<td></td>"
-        }
-        table_html += "</tr>"
-      }
-  
-    table_html += table_end
-
-    return table_html
 
 }
